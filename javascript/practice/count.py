57879943526442e3f097bccc39be4ddd2a158408
Raniya Thayyil <raniyaperson123@gmail.com>
2023-04-12 14:50:00 +0530

    etsy

--- javascript/practice/count.py
+++
@@ -1,8 +0,0 @@
-arr=[1,2,3,4,3,34,332,45,3,4,0]
-x=3
-count=0
-for i in arr:
-    if i==3:
-        count+=1
-
-print(count)           
--- js_fifty_project/rotating_navigation/script.js
+++
@@ -1,9 +0,0 @@
-const open=document.getElementById('open')
-const close=document.getElementById('close')
-const container =document.querySelector('.container')
-
-open.addEventListener('dblclick',()=>
-    container.classList.add('show-nav'))
-close.addEventListener('dblclick',()=>{
-    container.classList.remove('show-nav')
-})
